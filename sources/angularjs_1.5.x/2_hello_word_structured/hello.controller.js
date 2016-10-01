4557b3bb3edd65cc88b7f58b8d98c9b60948192d
akaliza <florakalisa@yahoo.fr>
2016-10-01 21:30:52 +0200

    folders structure

---
+++ sources/angularjs_1.5.x/2_hello_word_structured/hello.controller.js
@@ -0,0 +1,9 @@
+angular.module('myapp')
+    .controller('HelloController', HelloController);
+
+HelloController.$inject = ['$scope'];
+
+function HelloController($scope) {
+    console.log('HelloController initialized...');
+    $scope.name;
+}
